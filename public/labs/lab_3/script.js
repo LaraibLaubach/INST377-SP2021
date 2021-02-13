91671a8918d467d7add3c311dc39eace0ee21473
Laraib Laubach <llaubach@terpmail.umd.edu>
2021-02-12 20:39:46 -0500

    Update W3 JS
    Used the vanila js slider, no idea if this will actually work.

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,28 @@
 /* Put your javascript in here */
+
+const imageArray = [];
+for (let i = 0; i < document.querySelectorAll('.carousel div').length; i++) {
+    imageArray.push(document.querySelectorAll('.carousel div')[i].dataset.background);
+}
+
+let currentImageIndex = -1;
+
+function advanceCarouselItem() {
+    currentImageIndex++;
+
+    if (currentImageIndex >= imageArray.length) {
+        currentImageIndex = 0;
+    }
+
+    document.querySelector('.image-container').style.cssText = 'background: url("' + imageArray[currentImageIndex] + '") no-repeat center center; background-size: cover;';
+
+    const elems = document.getElementsByClassName('caption');
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].style.cssText = 'opacity: 0;';
+    }
+
+    const currentCaption = document.querySelector('.caption-' + (currentImageIndex));
+        currentCaption.style.cssText = 'opacity: 1;';
+    }
+
+    let intervalID = setInterval(advanceCarouselItem, 3000);
