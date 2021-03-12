f47b064c030ba871b0a79bef4933f5bb119d0fd3
Laraib Laubach <llaubach@terpmail.umd.edu>
2021-03-12 18:08:11 -0500

    Update to Lab 5 Script.js file

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,5 +1,5 @@
 function mapInit() {
-  const mymap = L.map("mapid").setView([38.9897, -76.9378], 13);
+  const mymap = L.map("mapid").setView([38.987749, -76.941910], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
